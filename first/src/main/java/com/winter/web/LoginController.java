5a507ab4be60e184daf4f73ac58bfc072fcd5209
wintershii <wintershii@163.com>
2018-11-07 15:22:51 +0800

    first spring

--- first/src/main/java/com/winter/web/LoginController.java
+++ first/src/main/java/com/winter/web/LoginController.java
@@ -1,11 +1,14 @@
 package com.winter.web;
 
+import com.winter.domain.User;
 import com.winter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @Controller
 public class LoginController {
@@ -15,8 +18,28 @@ public class LoginController {
     public String loginPage(){
         return "login";
     }
+
     @RequestMapping(value = "/loginCheck.html")
-    public ModelAndView loginCheck(HttpServletRequest request){
-        return null;
+    public ModelAndView loginCheck(HttpServletRequest request, LoginCommand loginCommand){
+        boolean isValidUser = userService.hasMatchUser(loginCommand.getUserName()
+                ,loginCommand.getPassword());
+        if (!isValidUser){
+            return new ModelAndView("login","error","用户名或密码错误");
+        } else {
+            User user = userService.findUserByUserName(loginCommand.getUserName());
+            user.setLastIp(request.getLocalAddr());
+            user.setLastVisit(new Date());
+            userService.loginSucess(user);
+            request.getSession().setAttribute("user",user);
+            return new ModelAndView("main");
+        }
+
     }
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+
 }
