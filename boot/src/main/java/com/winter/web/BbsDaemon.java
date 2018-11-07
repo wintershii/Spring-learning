cff267e0e7f8169572362c9617349c23063b7c46
wintershii <wintershii@163.com>
2018-11-07 18:56:31 +0800

    first spring boot

---
+++ boot/src/main/java/com/winter/web/BbsDaemon.java
@@ -0,0 +1,21 @@
+package com.winter.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class BbsDaemon {
+
+    @RequestMapping("/")
+    public String index() {
+        return "欢迎光临小春论坛";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsDaemon.class,args);
+    }
+}
